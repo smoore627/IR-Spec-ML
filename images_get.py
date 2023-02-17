d48018a92637ffda392bafa7850f40b83999a443
smoore627 <97919035+smoore627@users.noreply.github.com>
2023-02-17 09:24:31 -0500

    Update images_get.py

--- images_get.py
+++ images_get.py
@@ -12,13 +12,23 @@ def resolveCAS(chemical_name):
     cas_num = chemicals.identifiers.CAS_from_any(chemical_name).replace('-','')
     return cas_num
 
+cas_res = resolveCAS(chemical_list[0])
+liquid_ir = 'https://webbook.nist.gov/cgi/cbook.cgi?Spec=C' + str(cas_res) + '&Index=2&Type=IR&Large=on'
+
+
 
 
 for item in chemical_list:
     cas_res = resolveCAS(item)
     liquid_ir = 'https://webbook.nist.gov/cgi/cbook.cgi?Spec=C' + str(cas_res) + '&Index=2&Type=IR&Large=on'
-    print(cas_res,': liquid ir: ', liquid_ir)
+    solution = 'https://webbook.nist.gov/cgi/cbook.cgi?Spec=C' + str(cas_res) + '&Index=1&Type=IR&Large=on'
+    solution_ir = 'https://webbook.nist.gov/cgi/inchi?Spec=C' + str(cas_res) + '&Index=2&Type=IR&Large=on'
+    gas_ir = 'https://webbook.nist.gov/cgi/inchi?Spec=C' + str(cas_res) + '&Index=1&Type=IR&Large=on'
     
-    
+    html_text = requests.get(liquid_ir).content
+
+    with open('onion.png', 'wb') as handler: 
+
+       handler.write(html_text) 
 
 
