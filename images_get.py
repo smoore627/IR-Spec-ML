f989ec560e32367afc124c0d4323ec0d05f0d801
smoore627 <stephen_moore1@student.uml.edu>
2023-02-24 15:28:48 -0500

    Onions

--- images_get.py
+++ images_get.py
@@ -6,29 +6,28 @@ chemical_list = ['benzene',
                 'p-xylene',
                 'toluene',
                 'hexane',
-                'isoborneol']
+                'isoborneol',
+                'methanol']
 
 def resolveCAS(chemical_name):
     cas_num = chemicals.identifiers.CAS_from_any(chemical_name).replace('-','')
     return cas_num
 
-cas_res = resolveCAS(chemical_list[0])
-liquid_ir = 'https://webbook.nist.gov/cgi/cbook.cgi?Spec=C' + str(cas_res) + '&Index=2&Type=IR&Large=on'
+
 
 
 
 
 for item in chemical_list:
     cas_res = resolveCAS(item)
-    liquid_ir = 'https://webbook.nist.gov/cgi/cbook.cgi?Spec=C' + str(cas_res) + '&Index=2&Type=IR&Large=on'
-    solution = 'https://webbook.nist.gov/cgi/cbook.cgi?Spec=C' + str(cas_res) + '&Index=1&Type=IR&Large=on'
-    solution_ir = 'https://webbook.nist.gov/cgi/inchi?Spec=C' + str(cas_res) + '&Index=2&Type=IR&Large=on'
-    gas_ir = 'https://webbook.nist.gov/cgi/inchi?Spec=C' + str(cas_res) + '&Index=1&Type=IR&Large=on'
-    
-    html_text = requests.get(liquid_ir).content
-
-    with open('onion.png', 'wb') as handler: 
-
-       handler.write(html_text) 
+    for i in range(3):
+        ir_graph_1 = 'https://webbook.nist.gov/cgi/cbook.cgi?Spec=C' + str(cas_res) + '&Index=' + str(i) + '&Type=IR&Large=on'
+        ir_graph_2 = 'https://webbook.nist.gov/cgi/inchi?Spec=C' + str(cas_res) + '&Index=' + str(i) + '&Type=IR&Large=on'
+        image_1 = requests.get(ir_graph_1).content
+        image_2 = requests.get(ir_graph_2).content
+        with open('IR Spec Images/' + str(cas_res) + '_' + str(i) + '_cb.png', 'wb') as handler:  # change to specific path eventually
+            handler.write(image_1) 
+        with open('IR Spec Images/' + str(cas_res) + '_' + str(i) + '_in.png', 'wb') as handler: 
+            handler.write(image_2)
 
 
